feefabc122fb0d65e4bd6042f4700efdce3b0948
sehyun-hwang <hwanghyun3@gmail.com>
2023-11-24 20:19:36 +0900

    ray head node with FastAPI

--- ray_test.py
+++ ray_test.py
@@ -1,25 +1,51 @@
 from collections import Counter
 import socket
 import time
+from subprocess import check_call
 
+import uvicorn
 import ray
+from fastapi import FastAPI
 
-ray.init(address='ray://localhost:10001')
+app = FastAPI()
 
-print('''This cluster consists of
-    {} nodes in total
-    {} CPU resources in total
-'''.format(len(ray.nodes()), ray.cluster_resources()['CPU']))
 
 @ray.remote
 def f():
     time.sleep(0.001)
+    print(
+        """This cluster consists of
+        {} nodes in total
+        {} CPU resources in total
+    """.format(
+            len(ray.nodes()), ray.cluster_resources()["CPU"]
+        )
+    )
     # Return IP address.
     return socket.gethostbyname(socket.gethostname())
 
-object_ids = [f.remote() for _ in range(10000)]
-ip_addresses = ray.get(object_ids)
 
-print('Tasks executed')
-for ip_address, num_tasks in Counter(ip_addresses).items():
-    print('    {} tasks on {}'.format(num_tasks, ip_address))
+@app.get("/")
+async def root():
+    object_ids = [f.remote() for _ in range(2)]
+    ip_addresses = ray.get(object_ids)
+
+    print("Tasks executed")
+    for ip_address, num_tasks in Counter(ip_addresses).items():
+        print("    {} tasks on {}".format(num_tasks, ip_address))
+    return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    check_call(["ray", "start", "--head"])
+    print("main")
+    ray.init(address="auto")
+    print(
+        """This cluster consists of
+        {} nodes in total
+        {} CPU resources in total
+    """.format(
+            len(ray.nodes()), ray.cluster_resources()["CPU"]
+        )
+    )
+    uvicorn.run(app, host="0.0.0.0", port=8000)
